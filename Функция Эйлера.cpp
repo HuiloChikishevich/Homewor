3f371516f0eda3946dcb4dc2ec24ebb3d7882f9c
HuiloChikishevich <andrejiljin2000@mail.ru>
2016-11-15 21:29:12 +0300

    123

--- Функция Эйлера.cpp
+++ Функция Эйлера.cpp
@@ -1,35 +1,31 @@
 #include <iostream>
-#include <conio.h>
-#include <vector>
-#include <math.h>
+
 
 using namespace std;
 
-void Eulers_formula(int a) {
-    int number_of_numbers = 1;
-    bool flag = 1;
-    for (int i = 1; i < a; i++){
-        for (int j = 2; j < 10; j ++){
-            if ((i%j == 0 & i != j) ^ a%i == 0){
-                flag = 0;
-                break;
-            }
-        }
-    if (flag == 1){
-        number_of_numbers++;
-    }
-    flag = 1;
- }
-    cout << number_of_numbers << endl;
+void Eulers_formula(int n) {
+	int x[n];
+	int y = n;
+	for (int i = 0; i < n; i++) {
+		x[i] = i + 1;
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = n; j > 0; j--) {
+			if (n%j == 0 & x[i]%j== 0)  {
+				if (j != 1) {
+					y--;
+				}
+				break;
+			}
+		}
+	}
+	cout << y;
 
 }
 
 int main() {
-	setlocale(0, "Russian");
-	int a;
-	cout << "¬ведите предел" << endl;
-	cin >> a;
-	Eulers_formula(a);
-	_getch();
+	int n;
+	cin >> n;
+	Eulers_formula(n);
 	return 0;
 }
